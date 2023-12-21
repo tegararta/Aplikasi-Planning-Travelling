71f3949fdad43252d96e65afcd1b0d419cc56edc
aisyadwi <aisyaazahra85@gmail.com>
2023-12-21 21:23:00 +0700

    update note & history

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,14 +1,158 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// History.jsx
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet, Button } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
-const History = () => {
+const History = ({ route }) => {
+  const { doneNote } = route.params;
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (doneNote) {
+      setCompletedTasks((prevTasks) => [...prevTasks, doneNote]);
+    }
+  }, [doneNote]);
+
+  const getNoteValue = (note, label) => {
+    const regex = new RegExp(`${label}: (.+)`);
+    const match = note.match(regex);
+    return match ? match[1] : '';
+  };
+  
+  const handleDelete = async (index) => {
+    const updatedTasks = [...completedTasks];
+    const deletedNote = updatedTasks.splice(index, 1)[0];
+    setCompletedTasks(updatedTasks);
+
+    // Simpan kembali ke AsyncStorage setelah menghapus
+    saveDataToAsyncStorage(updatedTasks);
+
+    // Untuk memberi kesempatan AsyncStorage menyimpan data sebelum kembali ke laman Note
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    // Navigasi kembali ke laman Note
+    navigation.navigate('Note');
+  };
+
+  const handleUndone = async (index, note) => {
+    // Hapus dari completedTasks
+    const updatedTasks = [...completedTasks];
+    updatedTasks.splice(index, 1);
+    setCompletedTasks(updatedTasks);
+
+    // Tambahkan kembali ke savedNotes
+    const savedNotes = await getDataFromAsyncStorage('savedNotes');
+    const updatedSavedNotes = [...savedNotes, note];
+    saveDataToAsyncStorage(updatedSavedNotes);
+
+    // Navigasi kembali ke laman Note
+    navigation.navigate('Note');
+  };
+
+  const saveDataToAsyncStorage = async (data) => {
+    try {
+      await AsyncStorage.setItem('completedTasks', JSON.stringify(data));
+    } catch (error) {
+      console.error('Error saving data to AsyncStorage:', error);
+    }
+  };
+
+  const getDataFromAsyncStorage = async (key) => {
+    try {
+      const data = await AsyncStorage.getItem(key);
+      return data ? JSON.parse(data) : [];
+    } catch (error) {
+      console.error('Error getting data from AsyncStorage:', error);
+      return [];
+    }
+  };
+
+  const renderNoteItem = ({ item, index }) => {
+    const tujuanTravelling = getNoteValue(item, 'Tujuan Travelling');
+    const budget = getNoteValue(item, 'Budget');
+    const kebutuhanTravelling = getNoteValue(item, 'Kebutuhan Travelling');
+    const tanggal = getNoteValue(item, 'Tanggal');
+  
+    return (
+      <View style={styles.task}>
+        <Text style={styles.itemList}>
+          <Text style={styles.itemLabel}>Tujuan Travelling:</Text> {tujuanTravelling}
+        </Text>
+        <Text style={styles.itemList}>
+          <Text style={styles.itemLabel}>Budget:</Text> Rp.{budget}
+        </Text>
+        <Text style={styles.itemList}>
+          <Text style={styles.itemLabel}>Kebutuhan Travelling:</Text> {kebutuhanTravelling}
+        </Text>
+        <Text style={styles.itemList}>
+          <Text style={styles.itemLabel}>Tanggal:</Text> {tanggal}
+        </Text>
+        <View style={styles.buttonsContainer}>
+          <Button
+            title="Delete"
+            onPress={() => handleDelete(index)}
+            color="#e74c3c"
+          />
+          <Button
+            title="Undone"
+            onPress={() => handleUndone(index, item)}
+            color="#3498db"
+          />
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>History</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Riwayat Destinasi</Text>
+      <FlatList
+        data={completedTasks}
+        renderItem={renderNoteItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default History
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    marginTop: 10,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: 'green',
+    textAlign: 'center',
+  },
+  task: {
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 8,
+    backgroundColor: '#8DCBF4', // Warna latar belakang diubah
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  itemList: {
+    fontSize: 19,
+    marginBottom: 10,
+  },
+  itemLabel: {
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around', // Penyesuaian agar tombol lebih terlihat dan terpusat
+  },
+});
 
-const styles = StyleSheet.create({})
+export default History;
--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,22 +1,52 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 const Note = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
   const [travelDestination, setTravelDestination] = useState('');
   const [budget, setBudget] = useState('');
   const [travelNeeds, setTravelNeeds] = useState('');
   const [travelDate, setTravelDate] = useState('');
   const [savedNotes, setSavedNotes] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
+  const [doneNotes, setDoneNotes] = useState([]);
+
+  // Fungsi untuk menavigasi ke laman history
+  const navigateToHistory = (params) => {
+    navigation.navigate('History', params);
+  };
+
+  useEffect(() => {
+    // Cek apakah ada data editNote dari properti route.params
+    const { editNote, editIndex } = route.params || {};
+    if (editNote && editIndex !== undefined) {
+      // Mengisi state dengan data dari editNote
+      const noteParts = editNote.split('\n');
+      const destination = noteParts[0].split(': ')[1];
+      const budgetValue = noteParts[1].split(': Rp.')[1];
+      const needs = noteParts[2].split(': ')[1];
+      const date = noteParts[3].split(': ')[1];
+
+      setTravelDestination(destination);
+      setBudget(budgetValue);
+      setTravelNeeds(needs);
+      setTravelDate(date);
+      setEditIndex(editIndex);
+    }
+  }, [route.params]);
+
+  const overallBackgroundColor = '#2793D8';
 
   const handleSaveNote = () => {
     if (editIndex !== null) {
       const editedNotes = [...savedNotes];
-      editedNotes[editIndex] = `Tujuan Travelling: ${travelDestination}\nBudget: $${budget}\nKebutuhan Travelling: ${travelNeeds}\nTanggal: ${travelDate}`;
+      editedNotes[editIndex] = `Tujuan Travelling: ${travelDestination}\nBudget: Rp.${budget}\nKebutuhan Travelling: ${travelNeeds}\nTanggal: ${travelDate}`;
       setSavedNotes(editedNotes);
       setEditIndex(null);
     } else {
-      const note = `Tujuan Travelling: ${travelDestination}\nBudget: $${budget}\nKebutuhan Travelling: ${travelNeeds}\nTanggal: ${travelDate}`;
+      const note = `Tujuan Travelling: ${travelDestination}\nBudget: Rp.${budget}\nKebutuhan Travelling: ${travelNeeds}\nTanggal: ${travelDate}`;
       setSavedNotes([...savedNotes, note]);
     }
 
@@ -41,71 +71,135 @@ const Note = () => {
     setEditIndex(index);
   };
 
-  const handleDeleteNote = (index) => {
+  const formatCurrency = (value) => {
+    // Format angka ke format mata uang Rupiah
+    return new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
+      minimumFractionDigits: 0,
+    }).format(value);
+  };
+
+  const getNoteValue = (note, label) => {
+    const regex = new RegExp(`${label}: (.+)`);
+    const match = note.match(regex);
+    return match ? match[1] : '';
+  };
+
+  const NoteRow = ({ label, value }) => (
+    <View style={styles.noteRow}>
+      <Text style={styles.noteLabel}>{label}:</Text>
+      {label === 'Budget' ? (
+        <Text style={styles.noteValue}>{value.includes('') ? value : `Rp.${value}`}</Text>
+      ) : (
+        <Text style={styles.noteValue}>{value}</Text>
+      )}
+    </View>
+  );
+  
+
+  const handleUndone = (index, note) => {
+    // Hapus dari doneNotes
+    const updatedDoneNotes = [...doneNotes];
+    updatedDoneNotes.splice(index, 1);
+    setDoneNotes(updatedDoneNotes);
+  
+    // Tambahkan kembali ke savedNotes
+    setSavedNotes((prevNotes) => [...prevNotes, note]);
+  
+    // Navigasi kembali ke laman Note
+    navigation.navigate('Note');
+  };
+  const handleDoneNote = (index) => {
     const editedNotes = [...savedNotes];
-    editedNotes.splice(index, 1);
+    const deletedNote = editedNotes.splice(index, 1)[0];
     setSavedNotes(editedNotes);
+
+    // Navigasi ke layar history dengan menyertakan data doneNote
+    navigateToHistory({ doneNote: deletedNote });
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>Plan Your Travelling</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Plan Your Travelling</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Tujuan Travelling"
-        value={travelDestination}
-        onChangeText={(text) => setTravelDestination(text)}
-      />
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Tujuan Travelling</Text>
+          <TextInput
+            style={[styles.input, { backgroundColor: 'white' }]}
+            placeholder="Kota"
+            value={travelDestination}
+            onChangeText={(text) => setTravelDestination(text)}
+          />
+        </View>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Budget (Rp.)"
-        value={budget}
-        onChangeText={(text) => setBudget(text)}
-        keyboardType="numeric"
-      />
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Budget (Rp.)</Text>
+          <TextInput
+            style={[styles.input, { backgroundColor: 'white' }]}
+            placeholder="Rp."
+            value={budget}
+            onChangeText={(text) => setBudget(text)}
+            keyboardType="numeric"
+          />
+        </View>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Kebutuhan Travelling"
-        value={travelNeeds}
-        onChangeText={(text) => setTravelNeeds(text)}
-      />
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Kebutuhan Travelling</Text>
+          <TextInput
+            style={[styles.input, { backgroundColor: 'white' }]}
+            placeholder="Barang"
+            value={travelNeeds}
+            onChangeText={(text) => setTravelNeeds(text)}
+          />
+        </View>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Tanggal (dd/mm/yyyy)"
-        value={travelDate}
-        onChangeText={(text) => setTravelDate(text)}
-      />
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Tanggal</Text>
+          <TextInput
+            style={[styles.input, { backgroundColor: 'white' }]}
+            placeholder="dd/mm/yyyy"
+            value={travelDate}
+            onChangeText={(text) => setTravelDate(text)}
+          />
+        </View>
 
-      <Button title={editIndex !== null ? 'Update' : 'Save'} onPress={handleSaveNote} color="#3498db" />
+        <Button title={editIndex !== null ? 'Update' : 'Save'} onPress={handleSaveNote} color="#3498db" />
 
-      <View style={styles.notesContainer}>
-        <Text style={styles.notesTitle}>Note Travelling Anda</Text>
-        <ScrollView>
-          {savedNotes.map((note, index) => (
-            <View key={index} style={styles.noteItem}>
-              <Text style={styles.noteText}>{note}</Text>
-              <View style={styles.noteButtons}>
-                <Button title="Edit" onPress={() => handleEditNote(index)} color="#27ae60" />
-                <Button title="Delete" onPress={() => handleDeleteNote(index)} color="#e74c3c" />
+        <View style={styles.notesContainer}>
+          <Text style={styles.notesTitle}>Note Travelling Anda</Text>
+          <FlatList
+            data={savedNotes}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+              <View style={[styles.noteItem, { backgroundColor: 'white' }]}>
+                <NoteRow label="Tujuan Travelling" value={getNoteValue(item, 'Tujuan Travelling')} />
+                <NoteRow label="Budget" value={`${getNoteValue(item, 'Budget')}`} />
+                <NoteRow label="Kebutuhan Travelling" value={getNoteValue(item, 'Kebutuhan Travelling')} />
+                <NoteRow label="Tanggal" value={getNoteValue(item, 'Tanggal')} />
+                <View style={styles.noteButtonsContainer}>
+                  <View style={styles.noteButton}>
+                    <Button title="Edit" onPress={() => handleEditNote(index)} color="#3498db" />
+                  </View>
+                  <View style={styles.noteButton}>
+                    <Button title="Done" onPress={() => handleDoneNote(index)} color="#e74c3c" />
+                  </View>
+                </View>
               </View>
-            </View>
-          ))}
-        </ScrollView>
+            )}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
-
-export default Note;
 
 const styles = StyleSheet.create({
   container: {
     padding: 20,
     backgroundColor: '#ecf0f1',
+    overallBackgroundColor: '#2793D8',
     borderRadius: 10,
     margin: 20,
     shadowColor: '#000',
@@ -113,6 +207,18 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 3,
     elevation: 5,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: 20,
+  },
+  inputContainer: {
+    marginBottom: 5,
+  },
+  inputLabel: {
+    fontSize: 15,
+    color: '#2c3e50',
+    marginBottom: 5,
   },
   titleText: {
     fontSize: 24,
@@ -138,6 +244,25 @@ const styles = StyleSheet.create({
     borderTopColor: '#bdc3c7',
     paddingTop: 10,
   },
+  noteRow: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  noteLabel: {
+    fontWeight: 'bold',
+    marginRight: 5,
+    width: 120, // Sesuaikan lebar label sesuai kebutuhan
+  },
+  noteValue: {
+    flex: 1,
+    color: '#2c3e50',
+  },
+  separator: {
+    width: 1,
+    height: '100%',
+    backgroundColor: '#bdc3c7',
+    marginHorizontal: 5,
+  },
   notesTitle: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -156,8 +281,10 @@ const styles = StyleSheet.create({
     color: '#2c3e50',
     marginBottom: 10,
   },
-  noteButtons: {
+  noteButtonsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
 });
+
+export default Note;
